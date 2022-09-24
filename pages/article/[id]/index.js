9337cfd45d7dd2b97faf1754c7fd674b79f36766
kihiuFrank <franklinekihiu@gmail.com>
2022-09-24 22:55:58 +0300

    done

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,8 +1,110 @@
 import { useRouter } from "next/router";
-const article = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return <div>This is article {id}</div>;
+import Link from "next/link";
+import { server } from "../../../config";
+import Meta from "../../../components/Meta";
+
+const article = ({ article }) => {
+  //   const router = useRouter();
+  //   const { id } = router.query;
+  return (
+    <>
+      <Meta title={article.title} description={article.excerpt} />
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
 };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${server}/api/articles`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// one way. Here, we fetch the data when we go to these pages.
+/* export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+}; */
+
+// the other way. Here, data is fetched during build. this is faster
+/* export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+// getStaticPaths is required for dynamic SSG (Static Site Generated) pages
+export const getStaticPaths = async () => {
+  const res = await fetch(``);
+
+  const articles = await res.json();
+
+  // get an array of the article ids
+  const ids = articles.map((article) => article.id);
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}; */
+
+// getStaticPaths is required for dynamic SSG (Static Site Generated) pages
+/* export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+
+  const articles = await res.json();
+
+  // get an array of the article ids
+  const ids = articles.map((article) => article.id);
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}; */
 
 export default article;
